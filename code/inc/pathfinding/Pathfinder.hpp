35ab4eb774698790623a6f67b301bdd2feaea06a
Alexamakans <fougner.alexander@gmail.com>
2021-04-23 12:36:01 +0200

    Performance increase, comments, and tidying up

--- code/inc/pathfinding/Pathfinder.hpp
+++ code/inc/pathfinding/Pathfinder.hpp
@@ -8,6 +8,33 @@ typedef std::vector<const Tile*> Path;
 
 class Pathfinder
 {
+private:
+	struct Node
+	{
+		const Tile* pTile;
+		// Current best guess for start -> this -> goal
+		float fScore;
+		// Currently known cheapest path for start -> this
+		float gScore;
+	};
+
+	// Used for openSet in Pathfinder::CalculatePath
+	struct NodeCmpAscendingByFScore {
+		bool operator() (const Node* a, const Node* b) const
+		{
+			if (a->fScore == b->fScore)
+			{
+				if (a->gScore == b->gScore)
+				{
+					return a->pTile < b->pTile;
+				}
+
+				return a->gScore < b->gScore;
+			}
+
+			return a->fScore < b->fScore;
+		}
+	};
 public:
 	Pathfinder();
 	virtual ~Pathfinder();
@@ -17,12 +44,14 @@ public:
 	bool CalculatePath(const Tile& start, const Tile& goal, _Inout_ Path* pOutPath);
 
 private:
+	void ReconstructPath(const Tile* pTile, std::unordered_map<const Tile*, const Tile*> cameFrom, _Inout_ Path* pOutPath);
+	Node* GetOrCreateNode(const Tile* pTile);
+	const Tile* GetUsingOffset(const Tile* pTile, int32 dx, int32 dy);
+	bool ValidateTile(const Tile* pTile);
+
 	// World to pathfind in.
 	World* m_pWorld;
 
-	// Currently known cheapest path.
-	std::unordered_map<const Tile*, float> gScore;
-
-	// Current best guess.
-	std::unordered_map<const Tile*, float> fScore;
+	// Used to relate Tiles with Node data: fScore, gScore
+	std::unordered_map<const Tile*, Node*> m_TileNodeMappings;
 };
--- code/src/pathfinding/Pathfinder.cpp
+++ code/src/pathfinding/Pathfinder.cpp
@@ -20,114 +20,166 @@ void Pathfinder::SetWorld(World* pWorld)
 	m_pWorld = pWorld;
 }
 
-float h(const Tile& a, const Tile& b)
+// Leads to paths with fewer turns
+float ManhattanDistance(const Tile& a, const Tile& b)
 {
 	return fabsf(static_cast<float>(a.GetX() - b.GetX())) + fabsf(static_cast<float>(a.GetY() - b.GetY()));
-	//return sqrtf(static_cast<float>((a.GetX() - b.GetX()) * (a.GetX() - b.GetX()) + (a.GetY() - b.GetY()) * (a.GetY() - b.GetY())));
+}
+
+// Tends to zig-zag a lot when the goal is in a diagonal direction
+float EuclideanDistance(const Tile& a, const Tile& b)
+{
+	// sqrtf not needed here, but without it it wouldn't really be Euclidean Distance, so I'm leaving it in.
+	return sqrtf((a.GetX() - b.GetX()) * (a.GetX() - b.GetY()) + (a.GetY() - b.GetY()) * (a.GetY() - b.GetY()));
+}
+
+float h(const Tile& a, const Tile& b)
+{
+	return ManhattanDistance(a, b);
 }
 
 bool Pathfinder::CalculatePath(const Tile& start, const Tile& goal, _Inout_ Path* pOutPath)
 {
-	// Currently known cheapest path
-	std::unordered_map<const Tile*, float> gScore;
-	gScore.insert_or_assign(&start, 0.0f);
+	Node* pStart = new Node();
+	pStart->pTile = &start;
+	pStart->gScore = 0.0f; // start -> start has no cost
+	pStart->fScore = h(start, goal);
 
-	// Current best guess
-	std::unordered_map<const Tile*, float> fScore;
-	fScore.insert_or_assign(&start, h(start, goal));
+	m_TileNodeMappings.insert_or_assign(pStart->pTile, pStart);
 
-	std::set<const Tile*> openSet;
-	openSet.insert(&start);
+	// Contains Nodes of interest, ordered in ascending order by fScore (1st) and gScore (2nd)
+	std::set<Node*, NodeCmpAscendingByFScore> openSet;
+	openSet.insert(pStart);
 
 	std::unordered_map<const Tile*, const Tile*> cameFrom;
 
 	while (openSet.size() > 0)
 	{
-		// Get lowest fScore in openSet
-		const Tile* pCurrent = nullptr;
-		float lowest = std::numeric_limits<float>::infinity();
+		// Get Node with lowest fScore in openSet
+		Node* pCurrentNode = *openSet.begin();
 
-		for (const Tile* pTile : openSet)
+		// Path found?
+		if (pCurrentNode->pTile == &goal)
 		{
-			if (fScore.find(pTile) != fScore.end())
-			{
-				float score = fScore.at(pTile);
-				if (score < lowest)
-				{
-					lowest = score;
-					pCurrent = pTile;
-				}
-			}
-		}
+			ReconstructPath(pCurrentNode->pTile, cameFrom, pOutPath);
 
-		if (pCurrent == nullptr)
-		{
-			return false;
-		}
+			// deallocate nodes
+			for (const std::pair<const Tile*, Node*>& pair : m_TileNodeMappings)
+				delete pair.second;
 
-		if (pCurrent == &goal)
-		{
-			pOutPath->clear();
-			pOutPath->push_back(pCurrent);
-
-			auto it = cameFrom.find(pCurrent);
-			while (it != cameFrom.end())
-			{
-				pCurrent = it->second;
-				pOutPath->insert(pOutPath->begin(), pCurrent);
-				it = cameFrom.find(pCurrent);
-			}
+			m_TileNodeMappings.clear();
 
 			return true;
 		}
 
-		openSet.erase(pCurrent);
+		// processing this node now, can remove it from the open set
+		openSet.erase(pCurrentNode);
+
 		int32 offsets[4][2] = {
-			{ -1,  0 },
-			{  1,  0 },
-			{  0, -1 },
-			{  0,  1 },
+			{ -1,  0 }, // left
+			{  1,  0 }, // right
+			{  0, -1 }, // top
+			{  0,  1 }, // bottom
 		};
 
+		// For each orthogonal neighbor
 		for (uint16 i = 0; i < 4; ++i)
 		{
-			const Tile* pNeighbor = m_pWorld->GetTile(
-				pCurrent->GetX() + offsets[i][0],
-				pCurrent->GetY() + offsets[i][1]
-			);
+			const Tile* pNeighbor = GetUsingOffset(pCurrentNode->pTile, offsets[i][0], offsets[i][1]);
 
-			if (!pNeighbor)
+			if (!ValidateTile(pNeighbor))
 				continue;
 
-			float tmpGscore;
-			if (pNeighbor->flags & TILE_FLAG_UNWALKABLE)
-			{
-				tmpGscore = std::numeric_limits<float>::infinity();
-			}
-			else {
-				tmpGscore = gScore.at(pCurrent) + h(*pCurrent, *pNeighbor);
-			}
+			Node* pNeighborNode = GetOrCreateNode(pNeighbor);
 
-			float gS;
-			if (gScore.find(pNeighbor) != gScore.end())
-			{
-				gS = gScore.at(pNeighbor);
-			}
-			else {
-				gS = std::numeric_limits<float>::infinity();
-			}
+			// cost(start, current) + cost(current, neighbor) = cost(start, neighbor)
+			float tmpGscore = pCurrentNode->gScore + h(*pCurrentNode->pTile, *pNeighbor); // could add weight related things here, multiplying the result of h() by the weight
 
-			if (tmpGscore <= gS)
+			// Have we found a faster or same cost path to neighbor tile?
+			if (tmpGscore <= pNeighborNode->gScore)
 			{
-				// Found better path, record
-				cameFrom.insert_or_assign(pNeighbor, pCurrent);
-				gScore.insert_or_assign(pNeighbor, tmpGscore);
-				fScore.insert_or_assign(pNeighbor, tmpGscore + h(*pNeighbor, goal));
+				// Found better or equal path, record
+				// Store were we came from for reconstructing later.
+				cameFrom.insert_or_assign(pNeighbor, pCurrentNode->pTile);
 
-				openSet.insert(pNeighbor);
+				// start -> neighbor, best known
+				pNeighborNode->gScore = tmpGscore;
+
+				// start -> neighbor -> goal, best guess
+				pNeighborNode->fScore = tmpGscore + h(*pNeighbor, goal);
+
+				// Found a faster path than before, so we need to update the neighbor's neighbors as well
+				// i.e. this node is interesting
+				openSet.insert(pNeighborNode);
 			}
 		}
 	}
 
+	// deallocate nodes
+	for (const std::pair<const Tile*, Node*>& pair : m_TileNodeMappings)
+		delete pair.second;
+
+	m_TileNodeMappings.clear();
+
 	return false;
+}
+
+void Pathfinder::ReconstructPath(const Tile* pTile, std::unordered_map<const Tile*, const Tile*> cameFrom, Path* pOutPath)
+{
+	// Reconstruct path
+	pOutPath->clear();
+	pOutPath->push_back(pTile);
+
+	auto it = cameFrom.find(pTile);
+	while (it != cameFrom.end())
+	{
+		pTile = it->second;
+		pOutPath->insert(pOutPath->begin(), pTile);
+		it = cameFrom.find(pTile);
+	}
+}
+
+Pathfinder::Node* Pathfinder::GetOrCreateNode(const Tile* pTile)
+{
+	auto itNode = m_TileNodeMappings.find(pTile);
+	Node* pNeighborNode;
+	if (itNode == m_TileNodeMappings.end())
+	{
+		// No node exists for neighbor tile, create one
+
+		pNeighborNode = new Node();
+		pNeighborNode->pTile = pTile;
+		pNeighborNode->gScore = std::numeric_limits<float>::infinity();
+		pNeighborNode->fScore = pNeighborNode->gScore;
+
+		m_TileNodeMappings.insert_or_assign(pTile, pNeighborNode);
+	}
+	else {
+		// Node exists for neighbor tile, use it
+		pNeighborNode = itNode->second;
+	}
+
+	return pNeighborNode;
+}
+
+const Tile* Pathfinder::GetUsingOffset(const Tile* pTile, int32 dx, int32 dy)
+{
+	return m_pWorld->GetTile(
+		pTile->GetX() + dx,
+		pTile->GetY() + dy
+	);
+}
+
+bool Pathfinder::ValidateTile(const Tile* pTile)
+{
+	if (!pTile) // e.g. when out of bounds
+		return false;
+
+	if (pTile->flags & TILE_FLAG_UNWALKABLE)
+	{
+		// Can't walk on this tile, continue
+		return false;
+	}
+
+	return true;
 }
